5978c3cb7a54917a0cce9d6a037922a12a9cb27e
Maushmi Panda <mousumipanda946@gmail.com>
2024-11-27 12:32:53 +0530

    Update SignUpOne.java

--- src/main/java/bank/managementsystem/SignUpOne.java
+++ src/main/java/bank/managementsystem/SignUpOne.java
@@ -17,23 +17,23 @@ public class SignUpOne extends JFrame implements ActionListener {
     SignUpOne() {
         setLayout(null);
 
-        // Generate a random number for the form number
+        
         Random ran = new Random();
         random = Math.abs((ran.nextLong() % 9000L) + 1000L);
 
-        // Form Number Label
+        
         JLabel formno = new JLabel("APPLICATION FORM NO. " + random);
         formno.setFont(new Font("Raleway", Font.BOLD, 38));
         formno.setBounds(140, 20, 600, 40);
         add(formno);
 
-        // Personal Details Label
+        
         JLabel personDetails = new JLabel("Page 1: Personal Details");
         personDetails.setFont(new Font("Raleway", Font.BOLD, 22));
         personDetails.setBounds(290, 80, 400, 30);
         add(personDetails);
 
-        // Name Label and TextField
+        
         JLabel name = new JLabel("Name:");
         name.setFont(new Font("Raleway", Font.BOLD, 20));
         name.setBounds(100, 140, 100, 30);
@@ -44,7 +44,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         nameTextField.setBounds(300, 140, 400, 30);
         add(nameTextField);
 
-        // Father's Name Label and TextField
+       
         JLabel fname = new JLabel("Father's Name:");
         fname.setFont(new Font("Raleway", Font.BOLD, 20));
         fname.setBounds(100, 190, 200, 30);
@@ -55,7 +55,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         fnameTextField.setBounds(300, 190, 400, 30);
         add(fnameTextField);
 
-        // Date of Birth Label and DateChooser
+       
         JLabel dob = new JLabel("Date Of Birth:");
         dob.setFont(new Font("Raleway", Font.BOLD, 20));
         dob.setBounds(100, 240, 200, 30);
@@ -66,7 +66,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         dateChooser.setForeground(new Color(105, 105, 105));
         add(dateChooser);
 
-        // Gender Label and RadioButtons
+        
         JLabel gender = new JLabel("Gender:");
         gender.setFont(new Font("Raleway", Font.BOLD, 20));
         gender.setBounds(100, 290, 200, 30);
@@ -86,7 +86,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         genderGroup.add(male);
         genderGroup.add(female);
 
-        // Email Label and TextField
+       
         JLabel email = new JLabel("Email Address:");
         email.setFont(new Font("Raleway", Font.BOLD, 20));
         email.setBounds(100, 340, 200, 30);
@@ -97,7 +97,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         emailTextField.setBounds(300, 340, 400, 30);
         add(emailTextField);
 
-        // Marital Status Label and RadioButtons
+        
         JLabel marital = new JLabel("Marital Status:");
         marital.setFont(new Font("Raleway", Font.BOLD, 20));
         marital.setBounds(100, 390, 200, 30);
@@ -123,7 +123,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         maritalGroup.add(unmarried);
         maritalGroup.add(other);
 
-        // Address Label and TextField
+        
         JLabel address = new JLabel("Address:");
         address.setFont(new Font("Raleway", Font.BOLD, 20));
         address.setBounds(100, 440, 200, 30);
@@ -134,7 +134,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         addressTextField.setBounds(300, 440, 400, 30);
         add(addressTextField);
 
-        // City Label and TextField
+        
         JLabel city = new JLabel("City:");
         city.setFont(new Font("Raleway", Font.BOLD, 20));
         city.setBounds(100, 490, 200, 30);
@@ -145,7 +145,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         cityTextField.setBounds(300, 490, 400, 30);
         add(cityTextField);
 
-        // State Label and TextField
+       
         JLabel state = new JLabel("State:");
         state.setFont(new Font("Raleway", Font.BOLD, 20));
         state.setBounds(100, 540, 200, 30);
@@ -156,7 +156,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         stateTextField.setBounds(300, 540, 400, 30);
         add(stateTextField);
 
-        // Pincode Label and TextField
+        
         JLabel pincode = new JLabel("Pincode:");
         pincode.setFont(new Font("Raleway", Font.BOLD, 20));
         pincode.setBounds(100, 590, 200, 30);
@@ -167,7 +167,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         pinTextField.setBounds(300, 590, 400, 30);
         add(pinTextField);
 
-        // Next Button
+       
         next = new JButton("Next");
         next.setBackground(Color.BLACK);
         next.setForeground(Color.WHITE);
@@ -176,7 +176,7 @@ public class SignUpOne extends JFrame implements ActionListener {
         next.addActionListener(this);
         add(next);
 
-        // Frame properties
+       
         getContentPane().setBackground(Color.WHITE);
         setSize(850, 800);
         setLocation(350, 10);
